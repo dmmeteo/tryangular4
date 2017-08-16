0d573e4580143258332e8abd02d3a41890d46dd1
Dmitry Sichkar <dmmeteo@gmail.com>
2017-08-17 02:20:50 +0300

    6 - selectors & ngFor & ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http'
 
-import { AppComponent } from './app.component';
-import { VideoListComponent } from './video-list/video-list.component';
-import { VideoDetailComponent } from './video-detail/video-detail.component';
+import {AppComponent} from './app.component';
+import {VideoListComponent} from './video-list/video-list.component';
+import {VideoDetailComponent} from './video-detail/video-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VideoListComponent,
-    VideoDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        VideoListComponent,
+        VideoDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-video-list',
-  templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.css']
+    selector: 'app-video-list',
+    templateUrl: './video-list.component.html',
+    styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
+    title = 'List of videos';
+    // videoList = ['Item 1', 'Item 2', 'Item 3']; //LIST
+    videoList = [ //JSON
+        {name: 'Item 1', slug: 'item-1'},
+        {name: 'Item 2', slug: 'item-2'},
+        {name: 'Item 3', slug: 'item-3'}
+    ]
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
